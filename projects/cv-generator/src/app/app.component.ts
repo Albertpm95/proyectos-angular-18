f0420dfcaa5ff2219ffc86bbe7437ffbc8654be3
Albert <albertpm95@gmail.com>
2024-06-29 22:46:06 +0200

    Eliminados ficheros inutiles

--- projects/cv-generator/src/app/app.component.ts
+++ projects/cv-generator/src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
-import { LibreriaAngular18Component } from "../../../libreria-angular-18/src/lib/libreria-angular-18.component"
+import { LibreriaAngular18Component } from '../../../libreria-angular-18/src/lib/libreria-angular-18.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
-  imports: [RouterOutlet, LibreriaAngular18Component]
+  template: `<router-outlet /> `,
+
+  imports: [RouterOutlet, LibreriaAngular18Component],
 })
 export class AppComponent {
   title = 'cv-generator';
